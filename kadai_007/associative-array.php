c308818214c11be805a0c22ffed33cb88ee9a4e5
alice <37761958+solatama@users.noreply.github.com>
2024-07-28 21:19:08 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>kadai_007</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $vesitavle_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        print_r($vesitavle_data);
+
+        ?>
+    </p>
+</body>
