4a2b2b4221744da4bc3ee9899535c2458c5a2521
alice <37761958+solatama@users.noreply.github.com>
2024-08-03 15:14:48 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>kadai_011</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $vesitables = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
+
+        foreach ($vesitables as $key => $value) {
+            echo "{$key}:{$value} <br>";
+        }
+        ?>
+    </p>
+</body>
+</html>
